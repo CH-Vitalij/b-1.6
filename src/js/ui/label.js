f32b730d732ec94d453c8d2f96417f7ce7ecdd44
CH-Vitalij <v.charkin2017@gmail.com>
2024-04-25 17:17:42 +0500

    поправки для слайдера services

--- src/js/ui/label.js
+++ src/js/ui/label.js
@@ -1,47 +1,47 @@
 function label() {
   var readMore = document.querySelectorAll('.read-more')
   var label = document.querySelectorAll('.label')
-  var labelSpan  = []
-  var labelImg  = []
-  var aboutExcerpt  = document.querySelector('.about__excerpt')
-  var collapseRows  = document.querySelectorAll('.collapse__row')
+  var labelSpan = []
+  var labelImg = []
+  var aboutExcerpt = document.querySelector('.about__excerpt')
+  var collapseRows = document.querySelectorAll('.collapse__row')
   var arr = [aboutExcerpt]
 
-  var toggleElementBehavior = function (readMore, labelSpan , labelImg , arr) {
+  var toggleElementBehavior = function (readMore, labelSpan, labelImg, arr) {
     readMore.addEventListener('change', function () {
       if (readMore.checked) {
-        labelImg .style.transform = 'rotate(180deg)'
+        labelImg.style.transform = 'rotate(180deg)'
         arr.style.maxHeight = '1000px'
 
-        if (labelSpan .textContent === 'Читать далее') {
-          labelSpan .textContent = 'Свернуть'
+        if (labelSpan.textContent === 'Читать далее') {
+          labelSpan.textContent = 'Свернуть'
         } else {
-          labelSpan .textContent = 'Скрыть'
+          labelSpan.textContent = 'Скрыть'
         }
       } else {
-        labelImg .style.transform = ''
+        labelImg.style.transform = ''
         arr.style.maxHeight = ''
 
-        if (labelSpan .textContent === 'Свернуть') {
-          labelSpan .textContent = 'Читать далее'
+        if (labelSpan.textContent === 'Свернуть') {
+          labelSpan.textContent = 'Читать далее'
         } else {
-          labelSpan .textContent = 'Показать все'
+          labelSpan.textContent = 'Показать все'
         }
       }
     })
   }
 
   label.forEach((element) => {
-    labelSpan .push(element.querySelector('span'))
-    labelImg .push(element.querySelector('.label__img'))
+    labelSpan.push(element.querySelector('span'))
+    labelImg.push(element.querySelector('.label__img'))
   })
 
-  collapseRows .forEach((element) => {
+  collapseRows.forEach((element) => {
     arr.push(element)
   })
 
   readMore.forEach((element, i) => {
-    toggleElementBehavior(element, labelSpan [i], labelImg [i], arr[i])
+    toggleElementBehavior(element, labelSpan[i], labelImg[i], arr[i])
   })
 }
 
