21092e243a9be60f336afb4b41392849e6108b19
CH-Vitalij <v.charkin2017@gmail.com>
2024-04-26 15:43:42 +0500

    небольшие поправки и добавления блюра

--- src/js/modules/side-menu.js
+++ src/js/modules/side-menu.js
@@ -2,18 +2,24 @@ function sideMenu() {
   let upperMenuBurger = document.querySelector('.upper-menu__burger')
   console.log(upperMenuBurger)
 
-  let bodySideMenu = document.querySelector('.body__side-menu')
+  let bodySideMenu = document.querySelector('.side-menu__content')
   console.log(bodySideMenu)
 
   let upperMenuClosed = bodySideMenu.querySelector('.upper-menu__closed')
   console.log(upperMenuClosed)
 
+  let backgroundBlurred = document.querySelector(
+    '.side-menu__background-blurred'
+  )
+
   upperMenuBurger.addEventListener('click', function () {
     bodySideMenu.classList.toggle('side-menu_active')
+    backgroundBlurred.classList.toggle('side-menu_active')
   })
 
   upperMenuClosed.addEventListener('click', function () {
     bodySideMenu.classList.toggle('side-menu_active')
+    backgroundBlurred.classList.toggle('side-menu_active')
   })
 }
 
